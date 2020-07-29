e218d6f186f59a9f331115797ea9dc267f84539b
Tomas Pollak <tomaspollak@gmail.com>
2020-07-29 11:54:32 -0400

    More progress

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'bundler/setup'
-require 'omniauth'
+require 'omniauth-bootic'
 require 'sinatra'
 require 'bootic_client'
 
-class MiAplicacion < Sinatra::Base
+unless File.exist?('config.yml')
+  abort "config.yml missing. Please copy config.yml.example to config.yml and insert keys."
+end
+
+CONFIG = YAML.load_file('./config.yml')
+
+class ExampleApp < Sinatra::Base
 
   def initialize
     BooticClient.configure do |c|
-      c.client_id     = ENV['BOOTIC_KEY']
-      c.client_secret = ENV['BOOTIC_SECRET']
+      c.client_id     = CONFIG[:bootic_key]
+      c.client_secret = CONFIG[:bootic_secret]
       c.logger        = Logger.new(STDOUT)
       # c.cache_store = Rails.cache
     end
@@ -18,25 +24,14 @@ class MiAplicacion < Sinatra::Base
 
   set :views, './views'
   set :sessions, true
+
   use OmniAuth::Builder do
-    provider(:bootic, ENV['BOOTIC_KEY'], ENV['BOOTIC_SECRET'], { scope: 'public,admin' })
+    provider(:bootic, CONFIG[:bootic_key], CONFIG[:bootic_secret], { scope: 'public,admin' })
   end
 
   helpers do
     def current_shop
       @current_shop ||= root.shops.first
-    end
-
-    def current_user
-      @current_user ||= User.new(session[:auth_info])
-    end
-
-    def account_root
-      @account_root ||= client.from_hash(ACCOUNT_API)
-    end
-
-    def addons
-      @addons ||= account_root.addons(account_id: current_shop.account_id)
     end
 
     def root
@@ -47,6 +42,10 @@ class MiAplicacion < Sinatra::Base
       @client ||= BooticClient.client(:authorized, access_token: session[:access_token]) do |new_token|
         session[:access_token] = new_token
       end
+    end
+
+    def user_email
+      session[:auth_info][:email]
     end
 
     def logged_in?
@@ -61,38 +60,40 @@ class MiAplicacion < Sinatra::Base
     halt(500, err.message)
   end
 
-  get '/auth/:provider/callback' do
-    auth   = request.env['omniauth.auth']
-    params = request.env['omniauth.params']
-
-    # provider, uid = auth['provider'], auth['uid']
-    # session[:user_id] = uid
-
-    session[:auth_info]    = { name: auth['info']['name'], email: auth['info']['email'] }
-    session[:access_token] = auth['credentials']['token'] or raise "No credentials/token!"
-
-    url = session.delete(:return_to) || '/'
-    redirect to(url)
-  end
-
-  get '/auth/failure' do
-    redirect to('/login?message=failed')
-  end
-
-  helpers do
-
-  end
-
   get '/' do
-    erb :welcome
+    if logged_in?
+      redirect to('/admin')
+    else
+      erb :login
+    end
   end
 
   get '/admin' do
     if logged_in?
-      erb :index
+      erb :admin
     else
       redirect to('/')
     end
+  end
+
+  get '/logout' do
+    session.delete(:access_token)
+    session.delete(:auth_info)
+    redirect to('/')
+  end
+
+  get '/auth/:provider/callback' do
+    auth   = request.env['omniauth.auth']
+    params = request.env['omniauth.params']
+
+    session[:auth_info]    = { email: auth['info']['email'] } 
+    session[:access_token] = auth['credentials']['token'] or raise "No credentials/token!"
+
+    redirect to('/')
+  end
+
+  get '/auth/failure' do
+    redirect to('/login?error=oauth2_fail')
   end
 
 end
